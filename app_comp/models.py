eb4bf56e8c6020dbaf478222e3ea751e8598a71d
Walentin <seryimir@gmail.com>
2021-09-02 17:52:10 +0300

    add model 'Category'

--- app_comp/models.py
+++ app_comp/models.py
@@ -9,8 +9,28 @@ temp_bd = {'company': 'TorsionPLUS',
 class Category(db.Model):
     __tablename__ = 'categories'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(128), unique=True)
+    name = db.Column(db.String(128), unique=True)
     unit = db.Column(db.String(25), default=None)
 
     def __str__(self):
-        return f'-{self.id}-{self.title}'
+        return f'{self.id}: {self.name} ({self.unit})'
+
+
+"""
+class Component(db.Model):
+    __tablename__ = 'components'
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Float, unique=True, index=True, nullable=False)
+    unit = db.Column(db.String(25), default=None)   # ???
+    count = db.Column(db.Integer, default=0)
+    comment = db.Column(db.Text)
+
+    category = None
+    pattern = None
+
+    def __str__(self):
+        if self.unit:
+            return f'{self.value} {self.unit} {self.pattern}; count={self.count} id:({self.id})'
+        else:
+            return f'{self.value} {self.pattern}; count={self.count} id:({self.id})'
+"""
---
+++ component_depository/parsing_file_reports.py
@@ -0,0 +1,57 @@
+import csv
+import os
+import sys
+import pprint
+
+
+def find_file(type_file="csv"):
+    """select file with expansion (type_file) default=.csv
+    return list of all files
+    """
+    
+    lfile_=[File for File in os.listdir(r'.') if File.endswith(f'.{type_file}')]
+    # D={k:v for(k,v) in zip(range(1,(len(lfile)+1)),lfile)} # файлы с заданными ключами
+    return lfile_
+
+
+def select_unic_component(readed_object, pcb_name ) -> dict:
+    """
+    return list:
+        
+        [psb_name, {value1:{
+                        count:N, ComponentName:name, RefDes:rd, PatternName:pn },
+                    value2:{
+                        count:N, ComponentName:name, RefDes:rd, PatternName:pn }
+            }]
+    exmple:
+        ['U160TRANS-2.0', {'10MR 0.25W': {
+                                        'Count':'4', 
+                                        'ComponentName':'R_SMD_1210_M', 
+                                        'RefDes': 'R7',
+                                        'PatternName': 'R_SMD_1210_M'}
+            }]
+    """
+    
+    col_name = readed_object.__next__()
+    readed_object.__next__() # skip empty line
+    print(col_name)
+    pcb_values = dict()
+    for field in readed_object:
+        if field[0] and field[-1].lower()!='nm' and field[-1].lower()!='not mount':  # count not empty and value != nm
+            pcb_values[field[-1]] = {col_name[i]:field[i] for i in range(len(col_name)-1)}            
+    return [pcb_name, pcb_values]
+    
+
+if __name__ == "__main__":
+    report_file = find_file()[0]
+    print(report_file[:-4])
+    with open(report_file, newline='', encoding='utf-8', ) as object_csv:
+        rowreader = csv.reader(object_csv,  delimiter=';')
+        try:
+            pcb_object = select_unic_component(rowreader, report_file[:-4])
+        except csv.Error as e:
+            sys.exit(f'file {report_file}, line {rowreader.line_num}: {e}')
+        
+        pprint.pprint(pcb_object)
+
+    
