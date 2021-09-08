2bca3b51ebbd8b42aedb3e4b4f499080f921bcbf
Walentin <seryimir@gmail.com>
2021-09-08 17:49:09 +0300

    category

--- app_comp/models.py
+++ app_comp/models.py
@@ -9,11 +9,12 @@ temp_bd = {'company': 'TorsionPLUS',
 class Category(db.Model):
     __tablename__ = 'categories'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), unique=True)
-    unit = db.Column(db.String(25), default=None)
-
+    name = db.Column(db.String(128), unique=True, nullable=False)
+    unit = db.Column(db.String(50), default=None)   # Om, C , R, Hz
+    refdes = db.Column(db.String(15), nullable=False)
+    # components = db.Column(db.ForeignKey)
     def __str__(self):
-        return f'{self.id}: {self.name} ({self.unit})'
+        return f'{self.id}: {self.refdes} - {self.name} ({self.unit})'
 
 
 """
@@ -21,9 +22,11 @@ class Component(db.Model):
     __tablename__ = 'components'
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.Float, unique=True, index=True, nullable=False)
+    tolerance = db.Column(db.Float,)
     unit = db.Column(db.String(25), default=None)   # ???
-    count = db.Column(db.Integer, default=0)
+    count = db.Column(db.Integer, default=0)    
     comment = db.Column(db.Text)
+    
 
     category = None
     pattern = None
--- app_comp/views.py
+++ app_comp/views.py
@@ -1,9 +1,18 @@
-from app_comp import app
+from app_comp import app, db
 from flask import render_template
-from app_comp.models import temp_bd
+from app_comp.models import temp_bd, Category
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html", title="Home", bd=temp_bd)
+
+
+@app.route('/categories')
+def categories():
+    all_cat = db.session.query(Category).all()
+    return render_template("categories.html",
+                           title='Categories',
+                           all_categories=all_cat,
+                           bd=temp_bd)
