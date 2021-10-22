f5027e4ac698c69c897ba8e2d9e50a6964711636
Walentin <seryimir@gmail.com>
2021-10-22 17:38:25 +0300

    work with form component continue...

--- app_comp/forms.py
+++ app_comp/forms.py
@@ -14,7 +14,7 @@ class ComponentAddForm(FlaskForm):
     tolerance = IntegerField("Tol, %:")
     voltage = StringField("Voltage,V:", default=None)
     power = DecimalField('Power, Wt:', default=0.0)
-    count = IntegerField("Count:", default=0)
+    count = IntegerField("Count:", validators=[DataRequired()])
     comment = TextAreaField("Comment:")
     pattern = SelectField("Pattern:", choices=existing_patterns, validators=[DataRequired()])
     category = SelectField("Category:", choices=existing_categories, validators=[DataRequired()])
